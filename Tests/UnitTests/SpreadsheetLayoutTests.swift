573f917dbf91b59f419de239516e5766101239c5
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2019-05-23 18:07:27 -0400

    Remove the tests

--- Tests/UnitTests/SpreadsheetLayoutTests.swift
+++ Tests/UnitTests/SpreadsheetLayoutTests.swift
@@ -10,47 +10,4 @@ import XCTest
 @testable import SpreadsheetLayout
 
 final class SpreadsheetLayoutTests: XCTestCase {
-  private enum Identifier: String {
-    case Storyboard = "Main"
-    case ViewController = "spreadsheet"
-  }
-  
-  private var storyboard: UIStoryboard!
-  private var sut: SpreadsheetViewController!
-  
-  override func setUp() {
-    super.setUp()
-    
-    storyboard = UIStoryboard(name: Identifier.Storyboard.rawValue, bundle: Bundle.main)
-    sut = storyboard.instantiateViewController(withIdentifier: Identifier.ViewController.rawValue) as!
-    SpreadsheetViewController
-    
-    // This line gurantees that the initial view controller is the SpreadsheetViewController
-    UIApplication.shared.keyWindow!.rootViewController = sut
-  }
-  
-  override func tearDown() {
-    storyboard = nil
-    sut = nil
-    UIApplication.shared.keyWindow!.rootViewController = nil
-    
-    super.tearDown()
-  }
-  
-  func testStoryboardIsNotNil() {
-    XCTAssertNotNil(
-        UIStoryboard(name: Identifier.Storyboard.rawValue, bundle: Bundle.main)
-    )
-  }
-  
-  func testViewControllerIsNotNil() {
-    let storyboard = UIStoryboard(
-        name: Identifier.Storyboard.rawValue, bundle: Bundle.main
-    )
-    
-    let viewController = storyboard.instantiateViewController(
-        withIdentifier: Identifier.ViewController.rawValue
-      ) as! SpreadsheetViewController
-    
-    XCTAssertNotNil(viewController)
-  }}
+}
